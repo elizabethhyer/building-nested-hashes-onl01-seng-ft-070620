992391d7dbf73d9787884d845df7d0b2becf296c
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-10 18:38:58 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,8 +1,24 @@
-def second_challenge
-  epic_tragedy = {
-    :montague => { :patriarch => {}, :matriarch => {}, :hero => {} :hero_friends => [] }, 
-    :capulet => { :patriarch => {}, :matriarch => {}, :heroine => {}, :heroine_friends => [] }
-  }
+#def second_challenge
+ # epic_tragedy = {
+  #  :montague => { :patriarch => {}, :matriarch => {}, :hero => {} :hero_friends => [] }, 
+   # :capulet => { :patriarch => {}, :matriarch => {}, :heroine => {}, :heroine_friends => [] }
+  #}
 
   
-end
+#end
+
+epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
+ 
